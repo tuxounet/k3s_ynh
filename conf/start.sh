01d51ff9f98f8a4949e60bcfe882643585d46183
Mr Krux <krux@krux.fr>
2024-10-20 19:28:39 +0200

    ++

--- conf/start.sh
+++ conf/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
 
-__INSTALL_DIR__/main server --data-dir=__DATA_DIR__ --rootless --snapshotter=fuse-overlayfs --debug -v 10
+__INSTALL_DIR__/main server --data-dir=__DATA_DIR__   --debug -v 10
